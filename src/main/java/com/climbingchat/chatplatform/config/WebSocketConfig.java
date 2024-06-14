cde44a7e553aff0711d97643c7c4d4a97b0e5e30
ALLAVO <hjchokr@soongsil.ac.kr>
2024-06-14 19:35:20 +0900

    [feat] 실시간 채팅 기능 구현 완료

--- src/main/java/com/climbingchat/chatplatform/config/WebSocketConfig.java
+++ src/main/java/com/climbingchat/chatplatform/config/WebSocketConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
@@ -18,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")  // 또는 .setAllowedOrigins("http://localhost:8080") 사용
+                .setAllowedOriginPatterns("*") // allowedOriginPatterns를 사용하여 와일드카드 허용
                 .withSockJS();
     }
 }
---
+++ src/main/java/com/climbingchat/chatplatform/controller/WebSocketHandler.java
@@ -0,0 +1,17 @@
+package com.climbingchat.chatplatform.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        System.out.println("Received: " + payload);
+        session.sendMessage(new TextMessage("Echo: " + payload));
+    }
+}
